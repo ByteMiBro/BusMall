e7e32b11bef793161528e8b3e1e52ef4a44d630b
ByteMiBro <bytemibro@gmail.com>
2017-06-19 16:17:20 -0600

    images

--- js/model.js
+++ js/model.js
@@ -4,11 +4,31 @@ function ProductModel(name, price)
   this.name = name;
   this.price = price;
 }
+//goal is to store description in a variable of its own.
+function ProductDescription(description)
+{
+  this.description = description
+}
+var bootDescription = new ProductDescription('This is a thick rubber soled rain boot, designed and tested to keep your feet dry in the wetest conditions.');
+var chairDescription = new ProductDescription('This is a chair biuld by Rugma\'s in the east himalayan valley of Gustag. each grain of this nicely put together plastic chair was hadpicked. In this chair you will never fall');
+var scissorDescription = new ProductDescription('The blades on these scissors were forged by an elvish smith. They cut through even the thoughest papers.');
+var watercanDescription = new ProductDescription('this watering can is recomended by the scociety of watering women as its water threshold is the largest ever created.');
+var wineglassDescription = new ProductDescription('I don\'t drink ofted, but when I do I use this clear plastic wine glass.');
+var bootImage
+var
 var boots = new ProductModel('Boots', '$10.00');
 var chair = new ProductModel('Chair', '$20.00');
 var scissors = new ProductModel('Scissors', '$16.00');
 var waterCan = new ProductModel('Water Can', '$9.00');
 var wineGlass = new ProductModel('Wine Glass', '18.00');
+
+var listOfDescriptions = [
+  new ProductDescription('This is a thick rubber soled rain boot, designed and tested to keep your feet dry in the wetest conditions.'),
+  new ProductDescription('This is a chair biuld by Rugma\'s in the east himalayan valley of Gustag. each grain of this nicely put together plastic chair was hadpicked. In this chair you will never fall'),
+  new ProductDescription('The blades on these scissors were forged by an elvish smith. They cut through even the thoughest papers.'),
+  new ProductDescription('this watering can is recomended by the scociety of watering women as its water threshold is the largest ever created.'),
+  new ProductDescription('I don\'t drink ofted, but when I do I use this clear plastic wine glass.'),
+]
 
 var listOfProducts = [
   new ProductModel('Boots', '$10.00'),
--- md files/user_stories.md
+++ md files/user_stories.md
@@ -12,3 +12,13 @@ This is an app used in a focus group to collect data on user preferences of pote
 2.B. when on product is selected they can't select another product.
 2.C. can't go back sections
 3. collect data
+
+
+
+
+use hover event in html
+how would i use only html and css to show diferent images
+
+when user clicks vote button, display three randomised images next to each other with name and price of pictured item and
+when user hovers over that picture display its specific paragraph in a text box down below
+under each picture have a select radio button and at botom of page have vote button
