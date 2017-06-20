a9e11bf2938768e51ab9de5d846e28e76cf06181
ByteMiBro <bytemibro@gmail.com>
2017-06-20 13:06:48 -0600

    got chart working

--- js/product_destription.js
+++ js/product_destription.js
@@ -7,17 +7,46 @@ listOfProducts[i]
   var egami = listOfImages[i]
 }*/
 // objectives for today are creating a fake data set and a cchart
-var userData = [
-  {
-  name: 'boots'
+/*userData)= [
+ {
+ name: 'boots'
+ clicks: 21
+ },
+ {
+ name:'chair'
+ clicks: 9
+ },
+ {
+ name: 'scissors'
+ clicks: 2
+ },
+];*/
+var barData = {
+  labels: ['col1', 'col2', 'col3'],
+  datasets: [{
+    label: 'Bar Chart',
+    borderWidth: 1,
+    data: [
+      21,
+      9,
+      2
+    ]
+  }]
+};
+
+var userData = document.getElementById('userData').getContext('2d');
+ new Chart(userData, {
+   type: 'bar',
+   data: barData
+ });
+  /*name: 'boots',
   clicks: 21
   },
   {
-  name:'chair'
+  name:'chair',
   clicks: 9
   },
   {
-  name: 'scissors'
+  name: 'scissors',
   clicks: 2
-  },
-];
+});*/
