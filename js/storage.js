8ce662a78481533478a343eed6d9441bc1302b72
ByteMiBro <bytemibro@gmail.com>
2017-06-21 16:48:20 -0600

    started checking for value of button

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,27 @@
 'use strict';
 //the gaol is to store product selected after each vote to local storage
+/*var form = document.getElementById
+function checkStorageAvailability(){
+  if(){
+
+  }
+}*/
+var testForm = document.getElementById('awsome');
+
+testForm.addEventListener('radio', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var selectedTest = event.target.querySelector("input[name = 'vote']:checked");
+  var testValue = selectedTest && selectedTest.value;
+
+  var
+}
+
+function connectRadiotoPrduct(){
+
+}
 function addStoresToLocalStorage() {
   window.localStorage.stores = JSON.stringify(stores);
   console.log(window.localStorage.stores);
