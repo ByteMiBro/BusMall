d53304aceafc933b93f4830be991cb5c15002398
ByteMiBro <bytemibro@gmail.com>
2017-06-21 15:40:59 -0600

    added general function to bring jason out of his cave

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,6 @@
 'use strict';
+//the gaol is to store product selected after each vote to local storage
+function addStoresToLocalStorage() {
+  window.localStorage.stores = JSON.stringify(stores);
+  console.log(window.localStorage.stores);
+}
