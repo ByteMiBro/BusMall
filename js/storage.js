72d22aa1808e36faa22083841b74de13b20a228c
ByteMiBro <bytemibro@gmail.com>
2017-06-22 17:11:05 -0600

    added local storage

--- js/storage.js
+++ js/storage.js
@@ -1,30 +1,49 @@
 'use strict';
 //the gaol is to store product selected after each vote to local storage
-/*var form = document.getElementById
-function checkStorageAvailability(){
-  if(){
 
-  }
-}*/
-
+//grabs form from html
 var testForm = document.getElementById('test-form');
-
+//tells it to run through the event handler upon submiting
 testForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
+  //prevents entire form from submitting
   event.preventDefault();
+  //checks if value in radio button is selected
   var selectedTest = document.querySelector('input[name = "test"]:checked');
-//  for (j = 0 ; j < 3 ; j++) {
-    //var selectedTest = document.getElementByName('test').checked === j;
-
+  //sets a value for testValue
   var testValue = selectedTest && selectedTest.value;
+  //I can tell if it worked
   console.log(testValue);
 }
+function Form(name, click) {
+  this.name = name;
+  this.click = click || 0;
+}
 
-/*function connectRadiotoPrduct(){
+var forms = getFormsFromLocalStorage() || createDefaultForms();
+console.log(forms);
 
+function createDefaultForms() {
+  return [
+    new Form('one'),
+    new Form('Two')
+  ];
+}
+
+function getFormsFromLocalStorage() {
+  var formString = window.localStorage.forms;
+  if (!formString) {
+    return null;
+  }
+
+  var formsFromJson = JSON.parse(formString);
+  return valuesFromJson.map(function(formObj) {
+    console.log(formObj);
+    return new Product(formObj.clicks);
+  });
 }
 function addStoresToLocalStorage() {
-  window.localStorage.stores = JSON.stringify(stores);
-  console.log(window.localStorage.stores);
-}*/
+  window.localStorage.forms = JSON.stringify(forms);
+  console.log(window.localStorage.forms);
+}
